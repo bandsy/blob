bf81fa2810f706d8865646a23a219e31df31d259
Ryan Larkin <rlarkin212@gmail.com>
2020-05-15 23:23:22 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
+#[macro_use]
+extern crate log;
+
 use actix_web::{middleware, App, HttpServer};
+use dotenv::dotenv;
+use std::env;
 
 mod image;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let host = env::var("host").expect("host not set");
+    let port = env::var("port").expect("port not set");
+
+    info!("starting server");
+
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
             .configure(image::init_routes)
     })
-    .bind("127.0.0.1:8000")?
+    .bind(format!("{}:{}", host, port))?
     .run()
     .await
 }
