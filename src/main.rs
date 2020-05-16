689f793e4df21b2c2cec6f728e8af3f309ca9651
Ryan Larkin <rlarkin212@gmail.com>
2020-05-16 16:12:50 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,10 @@ mod image;
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let host = env::var("host").expect("host not set");
-    let port = env::var("port").expect("port not set");
+    //let host = env::var("host").expect("host not set");
+    //let port = env::var("port").expect("port not set");
+    let host = String::from("127.0.0.1");
+    let port = String::from("8000");
 
     HttpServer::new(|| {
         App::new()
